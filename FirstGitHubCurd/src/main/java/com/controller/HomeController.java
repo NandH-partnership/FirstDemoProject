ea8479d7951af707c36e9c7cce3c96b4e24d1f8c
shanihrj <44388629+shanihrj@users.noreply.github.com>
2018-10-23 15:33:14 +0530

    Update HomeController.java

--- FirstGitHubCurd/src/main/java/com/controller/HomeController.java
+++ FirstGitHubCurd/src/main/java/com/controller/HomeController.java
@@ -10,7 +10,7 @@ import com.services.Services;
 @Controller
 public class HomeController {
 @Autowired
-Services service;
+Services services;
 
 
 @RequestMapping("/")
@@ -22,35 +22,7 @@ public String start()
 public String start1()
 {
 	return "Registration";
-}
-=======
-import javax.persistence.Entity;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.model.Credentials;
-import com.model.User;
-import com.services.Services;
-
-@Entity
-public class HomeController {
-	@Autowired
-	private Services services;
-
-	public Services getServices() {
-		return services;
-	}
-
-	public void setServices(Services services) {
-		this.services = services;
-	}
->>>>>>> cf9c934f8925faa50bbdde6c415d37080ee064f6
-
-	@RequestMapping("/save")
+}	@RequestMapping("/save")
 	public String saveUser(@ModelAttribute User user,@ModelAttribute Credentials credentials
 			,Model model) {
 		int i=services.saveUser(user,credentials);
