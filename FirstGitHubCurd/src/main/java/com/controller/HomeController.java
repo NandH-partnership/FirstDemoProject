d702c2fab9cb6a8333e2b34ca219237655fdcafd
Your Name <you@example.com>
2018-10-23 13:57:17 +0530

    New Changes

--- FirstGitHubCurd/src/main/java/com/controller/HomeController.java
+++ FirstGitHubCurd/src/main/java/com/controller/HomeController.java
@@ -1,5 +1,26 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.services.Services;
+
+@Controller
 public class HomeController {
+@Autowired
+Services service;
+
+
+@RequestMapping("/")
+public String start()
+{
+	return "Login";
+}
+@RequestMapping("/registerpage")
+public String start1()
+{
+	return "Registration";
+}
 
 }
